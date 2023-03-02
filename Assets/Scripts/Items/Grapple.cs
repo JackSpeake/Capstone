7872539098397496610e5ed7becbdbc935cce6d8
Jack Speake <35775453+JackSpeake@users.noreply.github.com>
2023-03-02 11:57:50 -0500

    2 player grapple untested

--- Assets/Scripts/Items/Grapple.cs
+++ Assets/Scripts/Items/Grapple.cs
@@ -8,6 +8,8 @@ public class Grapple : MonoBehaviour
     [SerializeField] private float letGoDistance = 3f;
     [Tooltip("How much the player gets sped up each frame")]
     [SerializeField] private float speedUp = 1.005f;
+    [Tooltip("How much the player gets slowed down each frame")]
+    [SerializeField] private float slowDown = .995f;
 
     private GameObject player;
 
@@ -27,21 +29,47 @@ public class Grapple : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            objectHit = hit.transform;
-            Debug.Log("Cast hit: " + hit.transform.position);
-            NetworkCharacterControllerPrototype playerController = player.GetComponent<NetworkCharacterControllerPrototype>();
-
-            while ((player.transform.position - objectHit.position).sqrMagnitude > letGoDistance)
+            if (hit.transform.tag == "WALL")
             {
-                // TODO: Make player item manager know if/when the grapple starts or ends
-                // this would allow the player to have an early exit.
+                objectHit = hit.transform;
+                Debug.Log("Cast hit: " + hit.transform.position);
+                NetworkCharacterControllerPrototype playerController = player.GetComponent<NetworkCharacterControllerPrototype>();
 
-                Vector3 dir = objectHit.position - player.transform.position;
-                dir = dir.normalized;
-                playerController.VelMult = speedUp;
-                playerController.ShiftDirection(dir);
-                yield return 0;
+                while ((player.transform.position - objectHit.position).sqrMagnitude > letGoDistance)
+                {
+                    // TODO: Make player item manager know if/when the grapple starts or ends
+                    // this would allow the player to have an early exit.
+
+                    Vector3 dir = objectHit.position - player.transform.position;
+                    dir = dir.normalized;
+                    playerController.VelMult = speedUp;
+                    playerController.ShiftDirection(dir);
+                    yield return 0;
+                }
             }
+            else if (hit.transform.tag == "Player")
+            {
+                objectHit = hit.transform;
+                Debug.Log("Cast hit: " + hit.transform.position);
+                NetworkCharacterControllerPrototype playerController = player.GetComponent<NetworkCharacterControllerPrototype>();
+
+                while ((player.transform.position - objectHit.position).sqrMagnitude > letGoDistance)
+                {
+                    // TODO: Make player item manager know if/when the grapple starts or ends
+                    // this would allow the player to have an early exit.
+
+                    Vector3 dir = objectHit.position - player.transform.position;
+                    dir = dir.normalized;
+                    playerController.VelMult = speedUp;
+                    playerController.ShiftDirection(dir);
+
+                    dir *= -1;
+                    NetworkCharacterControllerPrototype otherPlayerController = hit.transform.gameObject.GetComponent<NetworkCharacterControllerPrototype>();
+                    otherPlayerController.VelMult = slowDown;
+                    otherPlayerController.ShiftDirection(dir);
+                    yield return 0;
+                }
+            }    
         }
 
         
