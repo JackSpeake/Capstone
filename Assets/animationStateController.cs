725777c10bd8eff67abde48bf821b36a66e5a294
Alexander Chen <chen.alexa@northeastern.edu>
2023-04-20 03:30:42 -0400

    Blend made but not working
    Gotta look into how network makes player controls maybe?

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator anim;
+    float speed = 0.0f;
+    float accel = 0.5f;
+    float decel = 0.5f;
+    float maxSpeed = 1f;
+    float minSpeed = 0f;
+    int velHash;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        Debug.Log("Animator Linked");
+        velHash = Animator.StringToHash("Speed");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // key input variables
+        bool forwardPress = Input.GetKey("w");
+
+        if (forwardPress && speed < maxSpeed)
+        {
+            speed += Time.deltaTime * accel;
+        } 
+        else if (!forwardPress && speed > minSpeed)
+        {
+            speed -= Time.deltaTime * decel;
+        } 
+        else if (speed < minSpeed)
+        {
+            speed = 0f;
+        }
+
+        anim.SetFloat(velHash, speed);
     }
 }
